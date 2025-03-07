4b32ca59952fcef80436c60dad3622810de71808
rdsteam18 <149048516+rdsteam18@users.noreply.github.com>
2025-03-08 02:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -54,17 +54,3 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error loading anime data:", error));
 });
- let url = encodeURIComponent(location.href);
-        document.querySelector(".whatsapp").href = `https://api.whatsapp.com/send?text=${url}`;
-        document.querySelector(".facebook").href = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
-        document.querySelector(".instagram").href = `https://www.instagram.com/?url=${url}`;
-        document.querySelector(".copy-url").onclick = () => {
-            navigator.clipboard.writeText(location.href).then(() => alert("URL copied!"));
-        };
-
-
-document.querySelectorAll('.title').forEach(el => {
-    if (el.innerText.length > 17) { 
-        el.innerText = el.innerText.substring(0, 17) + '...';
-    }
-});
